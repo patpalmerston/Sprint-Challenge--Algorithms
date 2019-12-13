2b9365b0a01a337bb77660c603e577e83e7629a2
Pat Palmerston <patpalmerston@gmail.com>
2019-12-13 09:44:36 -0800

    base case passes test

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,16 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
+  #Base Cases
+  #make sure word is a string
+    word =str(word)
     # TBC
+    #make sure word exists
+    if not word:
+        return 0
+
+    # check for length of word to be greater than one
+    if len(word) <=1:
+        return 0
     
     pass
